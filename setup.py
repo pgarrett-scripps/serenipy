a2dd0c3550bfdaa53377cba788b0551ab7c920c8
pgarrett <pgarrett@scripps.edu>
2022-11-20 17:01:21 -0800

    version 0.1.2

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="serenipy",
+    version="0.1.2",
+    author="Patrick Garrett",
+    author_email="pgarrett@scripps.edu",
+    description="A small package for handling ip2 related files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['serenipy'],
+    python_requires=">=3.6",
+    include_package_data=True,
+    package_data={"serenipy": ["*.dll"]}
+)
